e72722acfabacd8f78a4fb5279bd6f95af34fb01
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-07-18 23:30:47 +0900

    user詳細ページ user一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
   get 'pages/show'
-  get 'users/show'
   devise_for :users
   resources :posts
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
