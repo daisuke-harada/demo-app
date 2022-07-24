76261c25f4598dd6d4fe14a7343c886bf4132750
daisuke-harada <59969400+daisuke-harada@users.noreply.github.com>
2022-07-24 18:58:16 +0900

    feat: notificationの通知をoffに設定できるようにする。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @notifications = current_user.passive_notifications
-    @notifications.where(checked: false).each do |notification|
-      notification.update(checked: true)
-    end
+    # @notifications.where(checked: false).each do |notification|
+    #   notification.update(checked: true)
+    # end
   end
 
   def index
